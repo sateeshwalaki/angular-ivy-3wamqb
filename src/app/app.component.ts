5f4798f23a9273f3acfc3a9546a061e416ea74d2
sateeshwalaki <sateeshwalaki1998@gmail.com>
2021-07-21 11:10:44 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  title = "CodeSandbox";
+  enablebox()
+  {
+    this.disabled=false
+  }
 }
